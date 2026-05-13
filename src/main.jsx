46f98ad0d45b4a5ad340b049827ea787282ff54e
rgranadosd <rgranadosd@gmail.com>
2026-05-13 22:57:54 +0200

    Add Google Analytics GA4 con tracking por ruta

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,28 @@
-import { StrictMode } from 'react'
+import { StrictMode, useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import CalendarPage from './pages/CalendarPage.jsx'
 import TeamsPage from './pages/TeamsPage.jsx'
 
+function Analytics() {
+  const location = useLocation()
+  useEffect(() => {
+    if (typeof window.gtag === 'function') {
+      window.gtag('event', 'page_view', {
+        page_path: location.pathname,
+        page_title: document.title,
+      })
+    }
+  }, [location.pathname])
+  return null
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <Analytics />
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/calendario" element={<CalendarPage />} />
