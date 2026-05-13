8eacedea5ec667c6ba094da1d5d55fa0aefeac2a
rgranadosd <rgranadosd@gmail.com>
2026-05-13 17:08:54 +0200

    Set Vite base for custom domain

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/baybandits/',
+  base: '/',
   plugins: [react()],
   server: {
     port: 5173,
